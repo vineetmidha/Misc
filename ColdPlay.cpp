4d790bf638dd02998138f29b42004c822ad7b7d4
Vineet Midha <vineet.midha@yahoo.com>
2021-05-01 00:49:07 +0530

    CodeChef LunchTime April 2021

---
+++ ColdPlay.cpp
@@ -0,0 +1,20 @@
+
+// https://www.codechef.com/LTIME95C/problems/SLOOP
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	
+	while (t--)
+	{
+    	int m, s;
+    	cin >> m >> s;
+    	
+    	cout << m/s << endl;
+	}
+	
+	return 0;
+}
